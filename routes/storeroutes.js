d76686833959850d87db3185731165e405a02a29
Sukhija-Aniket <b20081@students.iitmandi.ac.in>
2022-06-14 00:41:58 +0530

    added changepassword function and deletefunction

--- routes/storeroutes.js
+++ routes/storeroutes.js
@@ -1,22 +1,27 @@
 const router = require("express").Router();
-const fs = require('fs');
+const fs = require("fs");
 const addressTable = require("../models/address");
 const productTable = require("../models/product");
 const categoryTable = require("../models/category");
-const multer = require('multer')
+const multer = require("multer");
 const { storeLoggedIn, storeCheck } = require("../middleware/auth");
-const { storeRegister,localStoreLogin} = require("../utils");
-const {addProduct,updateQuantity} = require("../utils/storeUtils");
+const { storeRegister, localStoreLogin } = require("../utils");
+const {
+  addProduct,
+  updateQuantity,
+  deleteProduct,
+  changePassword,
+} = require("../utils/storeUtils");
 const { findOne, findOneAndUpdate } = require("../models/category");
-const upload = multer({ 
+const upload = multer({
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads')
+      cb(null, "uploads");
     },
     filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now())
-    }
-  }) 
+      cb(null, file.fieldname + "-" + Date.now());
+    },
+  }),
 });
 const url = require("url");
 const { resetWatchers } = require("nodemon/lib/monitor/watch");
@@ -26,12 +31,12 @@ router.get("/register", storeLoggedIn, (req, res) => {
   res.render("store/register");
 });
 
-router.post("/register", async (req,res) => {
-  const message = await storeRegister(req,res);
-  if(message == "Password is too Short"){   
-    res.render("store/register", {message});
+router.post("/register", async (req, res) => {
+  const message = await storeRegister(req, res);
+  if (message == "Password is too Short") {
+    res.render("store/register", { message });
   } else {
-    res.redirect('/store/login');
+    res.redirect("/store/login");
   }
 });
 
@@ -58,9 +63,9 @@ router.get("/dashboard", storeCheck, (req, res) => {
 });
 
 router.get("/products", storeCheck, async (req, res) => {
-  const products = await productTable.find({store_id: req.user._id});
+  const products = await productTable.find({ store_id: req.user._id });
   const context = {
-    "products" : products
+    products: products,
   };
   // console.log(products);
   res.render("store/products", {
@@ -75,68 +80,84 @@ router.get("/addProduct", storeCheck, async (req, res) => {
   res.render("store/addProduct", {
     authenticated: req.isAuthenticated(),
     user: req.user,
-    category:category,
+    category: category,
   });
 });
 
-router.post("/addProduct", upload.single('image'), storeCheck, async (req, res) => {
-  req.body.image = {
-    data: fs.readFileSync('uploads/' + req.file.filename),
-    contentType: 'image/png'
+router.post(
+  "/addProduct",
+  upload.single("image"),
+  storeCheck,
+  async (req, res) => {
+    req.body.image = {
+      data: fs.readFileSync("uploads/" + req.file.filename),
+      contentType: "image/png",
+    };
+    const element = await categoryTable.findOne({
+      categoryName: req.body.categoryName,
+    });
+    req.body.store_id = req.user._id;
+    req.body.category_id = element._id;
+    await addProduct(req.body, "pending");
+    await element.update({ quantity: element.quantity + 1 });
+    res.redirect("/store/products");
   }
-  const element = await categoryTable.findOne({categoryName:req.body.categoryName});
-  req.body.store_id = req.user._id;
-  const message = await addProduct(req.body);
-  console.log(message);
-  await categoryTable.updateOne({categoryName:req.body.categoryName},{quantity:element.quantity+1});
+);
+
+router.get("/updateQuantity", storeCheck, async (req, res) => {
+  await updateQuantity(req,res);
   res.redirect("/store/products");
 });
 
-router.get("/updateProduct", storeCheck, async(req, res) => {
-  const {product_id, newQuantity} = url.parse(req.url, true).query;
-  await productTable.updateOne({_id: product_id},{availableQuantity: newQuantity});
-  res.send("Updated successfully");
-})
-
-router.get("/updateQuantity",storeCheck,async (req,res) => {
-  await updateQuantity({_id:req.body._id},{availableQuantity:req.body.quantity});
-  return; 
-})
-
 router.get("/deleteProduct", storeCheck, async (req, res) => {
-  var product_id = url.parse(req.url, true).query.ID;
-  await productTable.deleteOne({ _id: product_id});
+  await deleteProduct(req.res);
   res.redirect("/store/products");
+});
+
+router.post("/changePassword", storeCheck, async (req, res) => {
+  const message = await changePassword(req, res);
+  if (
+    message == "password is too Short." ||
+    message == "invalid password" ||
+    message == "passwords do not match"
+  ) {
+    console.log(message);
+    res.redirect("/store/dashboard");
+    // res.render("store/profile", {
+    //   authenticated: req.isAuthenticated(),
+    //   user: req.user,
+    //   message: message,
+    // });
+  }
+  res.redirect("/store/dashboard");
 });
 
 router.get("/profile", storeCheck, async (req, res) => {
-  console.log(req.user._id);
-  const address = await addressTable.findOne({user_id:req.user._id});
+  const address = await addressTable.findOne({ user_id: req.user._id });
   console.log(address);
   res.render("store/profile", {
     authenticated: req.isAuthenticated(),
     user: req.user,
-    address:address,
+    address: address,
   });
 });
 
 router.get("/contact", (req, res) => {
-  res.render("store/contact",{
-    user:req.user,
-    authenticated:req.isAuthenticated(),
+  res.render("store/contact", {
+    user: req.user,
+    authenticated: req.isAuthenticated(),
   });
 });
 
-
 router.get("/faqs", (req, res) => {
-  res.render("store/faq",{
-    user:req.user,
-    authenticated:req.isAuthenticated(),
+  res.render("store/faq", {
+    user: req.user,
+    authenticated: req.isAuthenticated(),
   });
 });
 
 // Logging Out
-router.get("/logout",storeCheck, (req, res) => {
+router.get("/logout", storeCheck, (req, res) => {
   req.logOut();
   res.redirect("/login");
 });
--- utils/storeUtils.js
+++ utils/storeUtils.js
@@ -1,10 +1,10 @@
-const storeTable = require("./models/store");
-const locationTable = require("./models/location");
-const categoryTable = require("./models/category");
-const productTable = require("./models/product");
-const userTable = require("./models/user");
-const addressTable = require("./models/address");
-const walletTable = require("./models/wallet");
+const storeTable = require("../models/store");
+const locationTable = require("../models/location");
+const categoryTable = require("../models/category");
+const productTable = require("../models/product");
+const userTable = require("../models/user");
+const addressTable = require("../models/address");
+const walletTable = require("../models/wallet");
 const bcrypt = require("bcryptjs");
 const req = require("express/lib/request");
 const passport = require("passport");
@@ -12,7 +12,7 @@ const url = require("url");
 var shortid = require("shortid");
 
 module.exports = {
-addProduct: async function (formData, status) {
+  addProduct: async function (formData, status) {
     const {
       name,
       store_id,
@@ -44,6 +44,30 @@ addProduct: async function (formData, status) {
       image: image,
     });
     return "Product Added Successfully";
+  },
+
+  changePassword: async function (req, res) {
+    const { password, newPassword,confirmPassword } = req.body;
+    if (newPassword == confirmPassword) {
+      const store = await storeTable.findOne({ _id: req.user._id });
+      const newEncryptedPassword = await bcrypt.hash(newPassword, 10);
+      if (newPassword.length < 8) return "password is too Short.";
+      if(bcrypt.compare(password,store.password)) {
+        await store.updateOne({ password: newEncryptedPassword });
+        return "Password changed Successfully.";
+      } else return "invalid password";
+    } else return "passwords do not match";
+  },
+
+  deleteProduct: async function (req, res) {
+    const product_id = url.parse(req.url, true).query.ID;
+    const product = await productTable.findOne({
+      _id: product_id,
+    });
+    const element = await categoryTable.findOne({_id:product.category_id});
+    await element.update({quantity:element.quantity-1});
+    await product.delete();
+    return "product deleted Successfully.";
   },
 
   updateQuantity: async function (req, res) {
